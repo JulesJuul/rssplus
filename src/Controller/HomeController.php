4c92e58d3c814a55465ca7435654e7462d34c2e1
Yan <souetre.yan@gmail.com>
2025-04-15 17:29:30 +0200

    feat: home informative message

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,16 +7,25 @@ use Symfony\Bundle\SecurityBundle\Security as SymfonySecurity;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\UserSource;
 
 class HomeController extends AbstractController
 {
     #[Route("/", name: "app_home")]
-    public function index(): Response
+    public function home(EntityManagerInterface $em): Response
     {
         $user = $this->getUser();
+        $userSources = [];
 
-        return $this->render("home/index.html.twig", [
-            "user" => $user,
+        if ($user) {
+            $userSources = $em->getRepository(UserSource::class)->findBy([
+                'user' => $user,
+            ]);
+        }
+
+        return $this->render('home/index.html.twig', [
+            'userSources' => $userSources,
         ]);
     }
 }
