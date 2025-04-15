1e7f954618f8b4762e5a7420e6dfc5508f8c05d9
Jules Sorensen <julessorens@gmail.com>
2025-04-15 12:02:30 +0200

    feat: css for login/register pages

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: ["./assets/**/*.js", "./templates/**/*.html.twig"],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"xp-wallpaper": "url('../images/windowsxp-background.jpg')",
+			},
+		},
+	},
+};
